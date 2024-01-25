69985be67c625440e1c26b802830ad1e48628cc8
vhcamposq <101605624+vhcamposq@users.noreply.github.com>
2024-01-25 15:16:07 -0300

    Implementando método GET de buscar informações do Cliente.

--- src/main/java/io/github/vhcamposq/cliente/rest/ClienteController.java
+++ src/main/java/io/github/vhcamposq/cliente/rest/ClienteController.java
@@ -2,9 +2,11 @@ package io.github.vhcamposq.cliente.rest;
 
 import io.github.vhcamposq.cliente.model.entity.Cliente;
 import io.github.vhcamposq.cliente.model.repository.ClienteRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -22,4 +24,9 @@ public class ClienteController {
         return repository.save(cliente);
 
     }
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+            return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
 }
