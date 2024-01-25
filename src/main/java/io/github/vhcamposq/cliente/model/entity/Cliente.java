59b8779d41cdfc40462d952f9873f32897f9201a
vhcamposq <101605624+vhcamposq@users.noreply.github.com>
2024-01-25 17:03:26 -0300

    Aplicando validações.

--- src/main/java/io/github/vhcamposq/cliente/model/entity/Cliente.java
+++ src/main/java/io/github/vhcamposq/cliente/model/entity/Cliente.java
@@ -2,6 +2,8 @@ package io.github.vhcamposq.cliente.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,10 +18,15 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotEmpty
     @Column(nullable = false, length = 150)
     private String nome;
+
+    @NotNull
     @Column(nullable = false, length = 11)
     private String cpf;
+
     @Column(name = "data_cadastro", updatable = false)
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
